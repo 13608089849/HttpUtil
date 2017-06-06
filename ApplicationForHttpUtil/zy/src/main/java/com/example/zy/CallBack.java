92044e91a8ff374ca038afc05db0fc00c11fa77c
13608089849 <1213814232@qq.com>
2017-06-06 09:58:57 +0800

    update

--- ApplicationForHttpUtil/zy/src/main/java/com/example/zy/CallBack.java
+++ ApplicationForHttpUtil/zy/src/main/java/com/example/zy/CallBack.java
@@ -13,23 +13,23 @@ public interface CallBack {
     /**
      * 成功时返回
      *
-     * @param string 需判空
+     * @param response 服务器返回字符串，需判空
      */
-    void onSuccess(String string);
+    void onSuccess(String response);
 
     /**
      * 失败时返回
      *
-     * @param string 需判空
-     * @param e      需判空
+     * @param message 错误信息，需判空
+     * @param e      异常，需判空
      */
-    void onFailure(String string, Exception e);
+    void onFailure(String message, Exception e);
 
     /**
      * 请求的信息
      *
-     * @param url        请求地址
-     * @param jsonObject 附加参数
+     * @param url    请求地址
+     * @param params 附加参数
      */
-    void onBefore(String url, JSONObject jsonObject);
+    void onBefore(String url, JSONObject params);
 }
